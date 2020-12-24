8bcf575266cdf2ff1e5dd25226de4c566e649568
artemmoroz <artemmoroz@coherentsolutions.com>
2020-12-24 18:31:51 +0300

    data files

---
+++ src/main/java/com/sparta/g4/files/DataTest.java
@@ -0,0 +1,69 @@
+package com.sparta.g4.files;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+public class DataTest {
+
+    public void doTest() throws IOException {
+
+        String p = "/Users/artemmoroz/Projects/training/sparta/src/main/resources/users.json";
+
+        File d = new File("src"+File.separator+"main"+File.separator+"resources"+File.separator+"users.json");
+
+        Path path = Paths.get("src", "main", "resources", "users.json");
+
+        File testuser = new File("testUser.json");
+
+        String content = new String(Files.readAllBytes(testuser.toPath()));
+        System.out.println(content);
+
+//        UserJson aaa = new ObjectMapper().readValue(this.getClass().getClassLoader().getResourceAsStream("users.json"), UserJson.class);
+//        System.out.println(aaa.getUsername());
+
+        List<UserJson> userJsons =
+                new ObjectMapper()
+                        .readValue(new File(p), new TypeReference<List<UserJson>>() {});
+        System.out.println(userJsons.get(1).getUsername());
+//
+//        UserJson artem2 = new UserJson();
+//        artem2.setUsername("sdgsdgsh");
+//        artem2.setIsmale(true);
+//        artem2.setAge(4363);
+//        artem2.setAaa("aaaa");
+//        artem2.setBbb("bbbbb");
+//
+//        new ObjectMapper()
+//                .writerWithDefaultPrettyPrinter()
+//                .writeValue(new File("testUser.json"), artem2);
+
+
+//        UserJson userJson = new ObjectMapper()
+//                .readValue(content, UserJson.class);
+//
+//        System.out.println(userJson.getPass());
+//        System.out.println(userJson.getKids());
+
+
+//        String[] strings = content.split(",");
+//
+//        String login = strings[0];
+//        String pass = strings[1];
+//
+//        System.out.println("login to the system "+login+" : "+ pass);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        new DataTest().doTest();
+    }
+
+}
---
+++ src/main/java/com/sparta/g4/files/UserJson.java
@@ -0,0 +1,37 @@
+package com.sparta.g4.files;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+@Data
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserJson {
+
+    String username;
+    String pass;
+    double age;
+    boolean ismale;
+    List<String> roles;
+    List<UserKids> kids;
+
+    @JsonProperty("xxxx")
+    String bbb;
+
+    @JsonIgnore
+    String aaa;
+
+    @Data
+    @Accessors(chain = true)
+    public static class UserKids{
+
+        String name;
+        int age;
+    }
+
+}
